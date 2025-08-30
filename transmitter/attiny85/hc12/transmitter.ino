da48891001f345f504b24c8adeaae838cef6f121
David Coita <david@coita.net>
2025-08-30 19:40:30 +0200

    feat: test better sleep

--- transmitter/attiny85/hc12/transmitter.ino
+++ transmitter/attiny85/hc12/transmitter.ino
@@ -1,13 +1,22 @@
 #include <SoftwareSerial.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
 #include "beacon.h"
 
-// RX, TX
 SoftwareSerial hc12(2, 1);
 
 BeaconPacket beacon = {
     .device_id = 0x00000001,
     .sequence_num = 0
 };
+
+int SLEEP_TIME = 60;
+
+volatile bool wakeup = false;
+
+ISR(WDT_vect) {
+  wakeup = true;
+}
 
 void setup() {
   CLKPR = 0x80;
@@ -17,11 +26,28 @@ void setup() {
   delay(100);
 }
 
+int getSleepCycles(int sleepTime) {
+  return sleepTime / 8;
+}
+
+void enterSleep() {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  // 8s sleep
+  WDTCR = (1<<WDE) | (1<<WDIE) | (1<<WDP3) | (1<<WDP0);
+  sleep_enable();
+  sleep_cpu();
+  sleep_disable();
+}
+
 void loop() {
   beacon.sequence_num++;
   hc12.write((uint8_t*)&beacon, sizeof(BeaconPacket));
-  delay(5000);
+  delay(100);
+  
+
+  int cycles = getSleepCycles(SLEEP_TIME);
+  
+  for(int i = 0; i < cycles; i++) {
+    enterSleep();
+  }
 }
-
-
-
--- transmitter/attiny85/nrf24/transmitter.ino
+++ transmitter/attiny85/nrf24/transmitter.ino
@@ -1,8 +1,9 @@
 #include <SPI.h>
 #include <RF24.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
 #include "beacon.h"
 
- // CE, CSN pin
 RF24 radio(2, 3);
 
 const byte address[5] = {0xE7, 0xE7, 0xE7, 0xE7, 0xE7};
@@ -11,6 +12,12 @@ BeaconPacket beacon = {
     .device_id = 0x00000001,
     .sequence_num = 0
 };
+
+volatile bool wakeup = false;
+
+ISR(WDT_vect) {
+  wakeup = true;
+}
 
 void setup() {
   CLKPR = 0x80;
@@ -22,8 +29,25 @@ void setup() {
   radio.stopListening();
 }
 
+void enterSleep() {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  WDTCR = (1<<WDE) | (1<<WDIE) | (1<<WDP3) | (1<<WDP0);
+  sleep_enable();
+  sleep_cpu();
+  sleep_disable();
+}
+
 void loop() {
   beacon.sequence_num++;
   radio.write(&beacon, sizeof(BeaconPacket));
-  delay(5000);
+  delay(100);
+  
+  radio.powerDown();
+  
+  for(int i = 0; i < 5; i++) {
+    enterSleep();
+  }
+  
+  radio.powerUp();
+  delay(10);
 }
