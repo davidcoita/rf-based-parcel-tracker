a73464e1783be5e596acc78dd67b06670270ff6f
David Coita <david@coita.net>
2025-09-18 16:52:50 +0200

    feat: fix esp8266 id

--- transmitter/esp8266/ble-hm10/transmitter.ino
+++ transmitter/esp8266/ble-hm10/transmitter.ino
@@ -1,16 +1,26 @@
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
 #include "beacon.h"
+
+#define EEPROM_ID_ADDR 0
 
 SoftwareSerial ble(D2, D1);
 
 BeaconPacket beacon = {
-    .device_id = 0x00000001,
+    .device_id = UNASSIGNED_ID,
     .sequence_num = 0
 };
 
 const int SLEEP_TIME = 60;
 
 void setup() {
+  EEPROM.begin(512);
+  
+  EEPROM.get(EEPROM_ID_ADDR, beacon.device_id);
+  if (beacon.device_id == 0 || beacon.device_id == 0xFFFFFFFF) {
+    beacon.device_id = UNASSIGNED_ID;
+  }
+  
   ble.begin(9600);
   delay(2000);
   
